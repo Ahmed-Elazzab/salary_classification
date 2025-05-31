80c9d2fdc0fa179acb5813610c8f091b78b472e1
Ahmed Elazab <ahmed@Ahmeds-MacBook-Air.local>
2025-05-31 23:33:01 +0300

    moving procfile and req to main folder

---
+++ starter/test_post.py
@@ -0,0 +1,23 @@
+import requests
+
+data = {
+    "age": 30,
+    "workclass": "State-gov",
+    "fnlgt": 141297,
+    "education": "Bachelors",
+    "education-num": 13,
+    "marital-status": "Married-civ-spouse",
+    "occupation": "Prof-specialty",
+    "relationship": "Husband",
+    "race": "Asian-Pac-Islander",
+    "sex": "Male",
+    "capital-gain": 0,
+    "capital-loss": 0,
+    "hours-per-week": 40,
+    "native-country": "India"
+}
+
+response = requests.post("https://immense-dusk-54638-1cdf10467aa5.herokuapp.com/predict", json=data)
+
+print(response.status_code)
+print(response.text)
