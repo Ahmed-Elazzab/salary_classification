1db413c8a62c7c65f21ec559ed9a9fa5cc584c93
Ahmed Elazab <ahmed@Ahmeds-MacBook-Air.local>
2025-05-31 18:50:37 +0300

    model training v1.3.0

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -1,4 +1,3 @@
-# Script to train machine learning model.
 from ml.model import train_model, inference, compute_model_metrics
 from ml.data import process_data
 from slicing import slice_metrics
@@ -15,10 +14,9 @@ sys.path.insert(0, file_dir)
 # Add code to load in the data.
 data = pd.read_csv(os.path.join(file_dir, '../data/clean_census.csv'))
 
-# Optional enhancement, use K-fold cross validation instead of a
 # train-test split.
 train, test = train_test_split(
-    data, test_size=0.2, random_state=42)  # added reproducibility
+    data, test_size=0.2, random_state=42)
 
 cat_features = [
     "workclass",
@@ -42,10 +40,15 @@ X_test, y_test, encoder, lb = process_data(
 # Train and save a model.
 model = train_model(X_train, y_train)
 
+# Ensure the model directory exists
+model_dir = os.path.join(file_dir, "../model")
+os.makedirs(model_dir, exist_ok=True)
+
 # Save artifacts
-pickle.dump(model, open("../model/model.pkl", "wb"))
-pickle.dump(encoder, open("../model/encoder.pkl", "wb"))
-pickle.dump(lb, open("../model/label_binarizer.pkl", "wb"))
+pickle.dump(model, open(os.path.join(model_dir, "model.pkl"), "wb"))
+pickle.dump(encoder, open(os.path.join(model_dir, "encoder.pkl"), "wb"))
+pickle.dump(lb, open(os.path.join(model_dir, "label_binarizer.pkl"), "wb"))
+
 
 # Inference and evaluation.
 train_pred = inference(model, X_train)
