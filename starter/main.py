f30a06eccf37f29a0ef30dbf7ed8b741bce1e01e
Ahmed Elazab <ahmed@Ahmeds-MacBook-Air.local>
2025-05-31 20:44:46 +0300

    setting up huruko

--- starter/main.py
+++ starter/main.py
@@ -9,6 +9,13 @@ from pydantic import BaseModel, Field
 from starter.ml.data import process_data
 from starter.ml.model import inference
 
+
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
+
 file_dir = os.path.dirname(os.path.realpath(__file__))
 
 model_path = os.path.join(file_dir, 'model/model.pkl')
